29319dc5fe5772e5e52e556a9c9faa9901c7dba0
Douglas Cantao <dcp.pereira@gmail.com>
2019-08-22 21:04:04 -0300

    Ajuste na porta do projeto

--- catalogo.py
+++ catalogo.py
@@ -425,4 +425,4 @@ def categoriaJSON():
 if __name__ == '__main__':
     app.secret_key = secret_key
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port='5000')
